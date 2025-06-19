d63852df5124d9b81ec51c53ebe222588ad33ad2
Harryj05 <harry@Harry.>
2025-06-20 04:57:57 +0530

    Change

--- error handling/main.go
+++ error handling/main.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func divide (a,b float64) (float64,error) {
-	if b==0{
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
 		return 0, fmt.Errorf("denominator must not be zero")
 	}
-	return a/b, nil
+	return a / b, nil
 }
 
-func main(){
+func main() {
 	fmt.Println("started error handling in the functṇion")
-	ans, _ := divide(10,0)
+	ans, _ := divide(10, 0)
 	fmt.Println("Division of two num", ans)
 }
--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "example.com/mymodule/myutil" // ✅ Import using module path
-		"example.com/mymodule/mydocx"
+	"fmt"
+
+	"example.com/mymodule/mydocx"
+	"example.com/mymodule/myutil" // ✅ Import using module path
 )
 
 func main() {
-    fmt.Println("Hello, World!")
-    myutil.PrintMessage("Hello, world")
-		mydocx.Printify("Go is good")
-		var name string ="harry"
-		fmt.Println(name)
-		var version = 76
-		fmt.Println(version)
-		var cur int = 45
-		var currency = 34.7
-		fmt.Println("Currency: ", currency)
-		fmt.Println("Currency: ", cur)
-		var dimension float64 = 798.23
-		fmt.Println("Float: ", dimension)
-		var person = "Harshit Jain"
-		fmt.Println(person)
+	fmt.Println("Hello, World!")
+	myutil.PrintMessage("Hello, world")
+	mydocx.Printify("Go is good")
+	var name string = "harry"
+	fmt.Println(name)
+	var version = 76
+	fmt.Println(version)
+	var cur int = 45
+	var currency = 34.7
+	fmt.Println("Currency: ", currency)
+	fmt.Println("Currency: ", cur)
+	var dimension float64 = 798.23
+	fmt.Println("Float: ", dimension)
+	var person = "Harshit Jain"
+	fmt.Println(person)
 
-		champ:="Champion"
-		fmt.Println(person)
-
-		var Public = "Data is important"//Initialize with cap letter to use in any package
-		var private = "data is private"//initialize with small letter to use in one package
-		
+	champ := "Champion"
+	fmt.Println(person)
+	fmt.Print(champ)
+	var Public = "Data is important" //Initialize with cap letter to use in any package
+	var private = "data is private"  //initialize with small letter to use in one package
+	fmt.Println(Public, private)
 
 }
