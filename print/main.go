1521ab3378f07447e7d4903c94cf2031d4594256
Harryj05 <harry@Harry.>
2025-06-20 02:14:36 +0530

    print

---
+++ print/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	age := 25
+	name := " Alice"
+	height := 5.55
+
+	fmt.Println("age: ", age, "height: ",height, "name", name)
+	fmt.Printf("age is %d",age)
+	fmt.Printf("height is %f\n",height)
+	fmt.Printf("Type of name is %T\n",name)
+	fmt.Printf("name is %s\n",)
+}
