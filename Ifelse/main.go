2b43364f8ae2d282646785e3d1514cdaee37a5de
Harryj05 <harry@Harry.>
2025-06-20 23:10:04 +0530

    Ifelse

---
+++ Ifelse/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	if x > 5 {
+		fmt.Println("x is greater")
+	} else {
+		fmt.Println("x is smaller")
+	}
+
+	y := 10
+	if x > 5 && y > 5 {
+		fmt.Println("hey how r you")
+	} else {
+		fmt.Println("Learn Programming")
+	}
+
+}
