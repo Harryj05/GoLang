7829f25f923da313b78f66b7a0427b797d4e08e7
Harryj05 <harry@Harry.>
2025-06-21 00:08:28 +0530

    Chnages

--- For loop/main.go
+++ For loop/main.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Numbers is :", i)
+	}
+
+	counter := 0
+	for {
+		fmt.Println("Infinite Loop")
+		counter++
+		if counter == 3 {
+			break
+		}
+	}
+
+	numbers := []int{1, 2, 3, 4, 5}
+	for index, value := range numbers {
+		fmt.Printf("Index of the Numbers is %d, and the value is %d\n", index, value)
+	}
+}
